0dfd038ac523a1f6a82f9bda2c1471bb4c4488cb
Zavicba <victorcva@gmail.com>
2023-07-13 21:55:08 -0300

    unicafe 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -10,6 +10,10 @@ const App = () => {
     const handleNeutral = () => setNeutral(neutral + 1)
     const handleBad = () => setBad(bad + 1)
 
+    const all = good + neutral + bad
+    const average = (good - bad) / all || 0
+    const positive = (good / all) * 100 || 0
+
     return (
         <div>
             <h1>give feedback</h1>
@@ -21,6 +25,9 @@ const App = () => {
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {all}</p>
+            <p>average {average}</p>
+            <p>positive {positive} %</p>
         </div>
     )
 }
