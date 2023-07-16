665f7c619e16ead8e4e59f0771e92728595b7d71
Zavicba <victorcva@gmail.com>
2023-07-16 19:43:46 -0300

    2.9 phoneBook step4

--- part2/phoneBook/src/App.js
+++ part2/phoneBook/src/App.js
@@ -1,12 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '1234567'}
-    ])
+    const personsData = [
+        {name: 'Arto Hellas', number: '040-123456'},
+        {name: 'Ada Lovelace', number: '39-44-5323523'},
+        {name: 'Dan Abramov', number: '12-43-234345'},
+        {name: 'Mary Poppendieck', number: '39-23-6423122'}
+    ]
+
+    const [persons, setPersons] = useState(personsData)
 
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [filteredPersons, setFilteredPersons] = useState('')
+
+    // I added this useEffect because I didn't like how the list behaved, it wasn't really dynamic. sorry if I ahead of a lesson
+    useEffect(() => {
+        if (filteredPersons.length < 1) {
+            setPersons(personsData)
+            return
+        }
+
+        let filteredPersonsByName = persons.filter(person => person.name.toLowerCase().includes(filteredPersons.toLowerCase()))
+        setPersons(filteredPersonsByName)
+    }, [filteredPersons])
 
     const handleSubmit = (event) => {
         event.preventDefault()
@@ -30,10 +47,18 @@ const App = () => {
         setNewNumber(event.target.value)
     }
 
+    const handleOnChangeFilter = (event) => {
+        setFilteredPersons(event.target.value)
+    }
+
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                <input onChange={handleOnChangeFilter}/>
+            </div>
 
+            <h2>Add a new</h2>
             <form>
                 <div>
                     name: <input onChange={handleOnChangeName}/>
